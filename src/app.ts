60534b2ff72f1794241c99a42b802c72361d6aac
Thiago Lima <tpl@cin.ufpe.br>
2022-10-08 22:28:47 +0100

    chore: add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import actuador from 'express-actuator'
+import helmet from 'helmet'
 
 const app = express()
+
+app.use(helmet())
 app.use(actuador())
 
 export default app
